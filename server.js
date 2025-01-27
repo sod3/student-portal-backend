8aaa62bcea61d60c0523a12df443c35e1a070d68
sod3 <mudassir.muneer123@gmail.com>
2025-01-27 17:07:28 +0500

    running

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
 // Routes
 app.use('/api/chat', chatRouter);
 app.use('/api/fees', feesRouter);
