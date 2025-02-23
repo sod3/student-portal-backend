b5010e85dc10608d329f9b3c7108a3114f84f2dc
sod3 <102423565+sod3@users.noreply.github.com>
2025-02-23 11:26:23 +0500

    Update announcementRoutes.js

--- routes/announcementRoutes.js
+++ routes/announcementRoutes.js
@@ -9,17 +9,17 @@ import {
   sendAnnouncementEmail,
   getTeacherAnnouncements,
 } from '../controllers/announcementsController.js';
-import auth from '../middleware/auth.js';
+//import auth from '../middleware/auth.js';
 
 const announcementsRouter = express.Router();
 
-announcementsRouter.post('/admin', auth, createAnnouncement);
-announcementsRouter.put('/admin/:id', auth, editAnnouncement);
-announcementsRouter.get('/admin', auth, viewAnnouncements);
-announcementsRouter.delete('/admin/:id', auth, deleteAnnouncement);
-announcementsRouter.get('/admin/filter', auth, filterAnnouncementsByDate);
-announcementsRouter.get('/student', auth, getStudentAnnouncements);
-announcementsRouter.post('/email', auth, sendAnnouncementEmail);
-announcementsRouter.get('/teacher', auth, getTeacherAnnouncements);
+announcementsRouter.post('/admin', createAnnouncement);
+announcementsRouter.put('/admin/:id', editAnnouncement);
+announcementsRouter.get('/admin', viewAnnouncements);
+announcementsRouter.delete('/admin/:id', deleteAnnouncement);
+announcementsRouter.get('/admin/filter', filterAnnouncementsByDate);
+announcementsRouter.get('/student', getStudentAnnouncements);
+announcementsRouter.post('/email', sendAnnouncementEmail);
+announcementsRouter.get('/teacher', getTeacherAnnouncements);
 
 export default announcementsRouter;
